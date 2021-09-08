67d5ca48951322228b77c742648f2bdb385ec90b
Paola3stefania <paolaestefania.decampos@gmail.com>
2021-09-09 01:22:58 +0200

    some content

--- src/index.js
+++ src/index.js
@@ -5,5 +5,20 @@ that your application needs (at least the module-a and module-b)
  */
 
 //import functions from modula a and module b
+import { CardContent, createButton } from "./js/module-a.js";
 
-alert("Webpack rocks!");
+const appDiv = document.getElementById("app");
+
+let card = new CardContent();
+card.createCard("First Card", "assets/img/mike-dorner.png", "banana", 1);
+card.appendCard(appDiv);
+
+let cardSec = new CardContent();
+cardSec.createCard("Second Card", "assets/img/carson-arias.jpg", "yellow plastic things", 2);
+cardSec.appendCard(appDiv);
+
+let cardThrd = new CardContent();
+cardThrd.createCard("Third Card", "assets/img/diego-ph.png", "hand holding a lightbulb", 3);
+cardThrd.appendCard(appDiv);
+
+createButton(appDiv);
--- src/js/module-a.js
+++ src/js/module-a.js
@@ -6,3 +6,44 @@ javascript module that makes use of the new features of ECMAScript 6 such as:
 *Make use of the Template Strings
 *Make use of Let and Const
  */
+
+
+
+export class CardContent {
+  constructor() {
+    this.newCard = document.createElement("div");
+    this.cardTitle = document.createElement("h2");
+    this.cardText = document.createElement("p");
+    this.cardImg = document.createElement("img");
+  }
+
+  createCard(title, imgSrc, imgAlt, number) {
+    this.cardTitle.textContent = title;
+    this.newCard.append(this.cardTitle);
+
+    this.cardImg.setAttribute("src", imgSrc);
+    this.cardImg.setAttribute("alt", imgAlt);
+    this.newCard.append(this.cardImg);
+
+    this.cardText.textContent = `This is the card number: ${number}`;
+    this.newCard.append(this.cardText);
+  }
+
+  appendCard(node) {
+    node.append(this.newCard);
+  }
+}
+
+export const createButton = (node) => {
+
+  let button = document.createElement("button");
+  button.textContent = "Say Hi";
+  button.addEventListener("click", sayHi);
+  node.insertAdjacentElement("beforeend", button);
+
+};
+
+const sayHi = () => {
+  alert("Hi");
+}
+
