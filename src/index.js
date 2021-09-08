d08063ab9f22d2578211a6a4f49b16691f41bb74
Paola3stefania <paolaestefania.decampos@gmail.com>
2021-09-08 19:07:59 +0200

    config file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
+// import everything from main.js in js folder
 /* A main.js file that will be your main
 Javascript file responsible for calling the modules
 that your application needs (at least the module-a and module-b)
  */
+
+//import functions from modula a and module b
+
+alert("Webpack rocks!");
