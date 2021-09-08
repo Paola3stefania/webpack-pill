78d85daab7a2aec1e07cbd1ed4f9434627c7bb5b
Paola3stefania <paolaestefania.decampos@gmail.com>
2021-09-09 00:28:34 +0200

    sass

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,17 @@ const path = require("path");
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
+  // dev tool to see what its going on in bundle.js file
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss|css)$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
   }
 };
